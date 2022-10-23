d478138ade87d21a81e1dff456101fab664f54c2
Demetrius <wh46tesmoke@gmail.com>
2022-10-23 21:22:22 +0300

    update banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Banner.css"
+import axios from "./axios";
+import requests from './Request';
 function Banner() {
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function fetchData(){
+             const request = await axios.get(requests.fetchNetflixOriginals);
+             setMovie(
+                 request.data.results[
+                     Math.floor(Math.random() * request.data.results.length - 1)
+                     ]
+             );
+             return request;
+        }
+        fetchData();
+    },[]);
+    console.log(movie);
+    function truncate(string, n) {
+            return string?.length > n ? string.substring(0, n -1 ) + '...' : string
+    }
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage: `url("https://t4.ftcdn.net/jpg/03/71/56/17/360_F_371561715_LVI4qVJ2hyWMDXdqJNGdktggEzjQuC15.jpg")`
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`
         }}>
             <div className='banner__contents'>
                 <h1 className='banner__title'>
@@ -13,8 +32,9 @@ function Banner() {
                 <div className='banner__buttons'>
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
-                    <h1 className='bannner__description'>
-                        This is a test description
+                    <h1 className='banner__description'>
+                        {truncate(`This is a test description This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`,100)
+                        }
                     </h1>
                 </div>
             </div>
